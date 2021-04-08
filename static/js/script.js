5927fa1731f780f391faaf372a8d389a54e02e6a
vinnieOrdobas <viniordobas@icloud.com>
2021-04-08 09:46:43 +0000

    Add training types dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge:"right"});
     $('.collapsible').collapsible();
+    $('select').formSelect();
     $('.datepicker').datepicker({
       format: "dd mmmm, yyyy",
       yearRange: 3,
