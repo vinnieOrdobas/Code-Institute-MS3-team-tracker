f6671999850f553085eb406ff77161fc52035a74
vinnieOrdobas <viniordobas@icloud.com>
2021-04-08 08:55:06 +0000

    Add task for input fields@

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge:"right"});
     $('.collapsible').collapsible();
+    $('.datepicker').datepicker({
+      format: "dd mmmm, yyyy",
+      yearRange: 3,
+      showClearBtn: true,
+      i18n: {
+        done: "Select"
+      }
+    });
   });
