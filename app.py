dd672d34e0bc1326fd8acd0d0e267c07e52dfbc4
vinnieOrdobas <viniordobas@icloud.com>
2021-05-14 12:19:31 +0000

    Complete cycle functino refactored, button controls for each cycle implemented

--- app.py
+++ app.py
@@ -267,68 +267,6 @@ def enroll_training(training_id):
     return redirect(url_for('get_trainings'))
 
 
-@app.route("/complete_training/<training_id>", methods=['GET', 'POST'])
-def complete_training(training_id):
-    # marks training as complete
-    if request.method == 'POST':
-        # finds training name
-        training_name = mongo.db.trainings.find_one(
-            {'_id': ObjectId(training_id)})['training_name']
-        # finds training type
-        training_type = mongo.db.trainings.find_one(
-            {'_id': ObjectId(training_id)})['training_type']
-        # finds students
-        students = mongo.db.users.find(
-            {'student': True})
-        for student in students:
-            trainings = student['trainings']
-            current_training = trainings.get(training_name)
-            if current_training is not None:
-                user = student['alias']
-                current_training[training_type]['completed'] = True
-                trainings.update(current_training)
-                trainings.pop(training_type)
-                # sets training to complete on user's history
-                mongo.db.users.update_one(
-                        {'alias': user},
-                        {'$set': {'trainings': trainings}})
-                # sets training to complete on training's record
-                submit = {"$set": {"complete_training": "True"}}
-                mongo.db.trainings.update_one({'_id': ObjectId(training_id)}, submit)
-    return redirect(url_for('get_trainings'))
-
-
-@app.route("/incomplete_training/<training_id>", methods=['GET', 'POST'])
-def incomplete_training(training_id):
-    # marks training as pending
-    if request.method == 'POST':
-        # finds training name
-        training_name = mongo.db.trainings.find_one(
-            {'_id': ObjectId(training_id)})['training_name']
-        # finds training type
-        training_type = mongo.db.trainings.find_one(
-            {'_id': ObjectId(training_id)})['training_type']
-        # finds students
-        students = mongo.db.users.find(
-            {'student': True})
-        for student in students:
-            trainings = student['trainings']
-            current_training = trainings.get(training_name)
-            if current_training is not None:
-                user = student['alias']
-                current_training[training_type]['completed'] = False
-                trainings.update(current_training)
-                trainings.pop(training_type)
-                # sets training to pending on user's history
-                mongo.db.users.update_one(
-                        {'alias': user},
-                        {'$set': {'trainings': trainings}})
-                # sets training to pending on training's record
-                submit = {"$set": {"complete_training": "False"}}
-                mongo.db.trainings.update_one({'_id': ObjectId(training_id)}, submit)
-    return redirect(url_for('get_trainings'))
-
-
 # --------------------------------Cycle functions---------------------- #
 @app.route("/add_cycle/<training_id>", methods=['GET', 'POST'])
 def add_cycle(training_id):
@@ -371,6 +309,63 @@ def add_cycle(training_id):
     return render_template('add_cycle.html',
         instructors=instructors, training=training,
         training_types=training_types)
+
+
+@app.route("/complete_cycle/<training_id>", methods=['GET', 'POST'])
+def complete_training(training_id):
+    # marks training as complete
+    if request.method == 'POST':
+        # finds training name
+        training_name = mongo.db.trainings.find_one(
+            {'_id': ObjectId(training_id)})['training_name']
+        current_cycle = request.form.get('cycle_name')
+        students = mongo.db.users.find(
+            {f"trainings.{training_name}": {'$exists': "true"}})
+        # finds students
+        for student in students:
+            user = student['alias']
+            mongo.db.users.update_one(
+                {'alias': user},
+                {'$set':
+                    {f'trainings.{training_name}.{current_cycle}.completed':
+                        True}})
+#       sets training to complete on training's record
+        mongo.db.trainings.update_one(
+                {'_id': ObjectId(training_id)},
+                {'$set':
+                    {f'training_cycle.{current_cycle}.completed': True}})
+    return redirect(url_for('get_trainings'))
+
+
+@app.route("/incomplete_training/<training_id>", methods=['GET', 'POST'])
+def incomplete_training(training_id):
+    # marks training as pending
+    if request.method == 'POST':
+        # finds training name
+        training_name = mongo.db.trainings.find_one(
+            {'_id': ObjectId(training_id)})['training_name']
+        # finds training type
+        training_type = mongo.db.trainings.find_one(
+            {'_id': ObjectId(training_id)})['training_type']
+        # finds students
+        students = mongo.db.users.find(
+            {'student': True})
+        for student in students:
+            trainings = student['trainings']
+            current_training = trainings.get(training_name)
+            if current_training is not None:
+                user = student['alias']
+                current_training[training_type]['completed'] = False
+                trainings.update(current_training)
+                trainings.pop(training_type)
+                # sets training to pending on user's history
+                mongo.db.users.update_one(
+                        {'alias': user},
+                        {'$set': {'trainings': trainings}})
+                # sets training to pending on training's record
+                submit = {"$set": {"complete_training": "False"}}
+                mongo.db.trainings.update_one({'_id': ObjectId(training_id)}, submit)
+    return redirect(url_for('get_trainings'))
 
 
 if __name__ == "__main__":
